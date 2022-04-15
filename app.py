213e047a3b88ba4ddb99e86a5dbd5ac0c5509ace
GregDrapala <98324003+GregDrapala@users.noreply.github.com>
2022-04-15 19:03:35 +0100

    Update

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from flask import Flask, render_template, request, json, url_for
 
+
 app = Flask(__name__)
 
-def read_rates():
-    file = open("rates.json", "r")
-    content = file.read()
-    file.close()
 
+with open('rates.json', 'r') as f:
+    content= f.read()
+
+def read_rates():
     data = json.loads(content)
     return data["rates"]
 
-def getRatesByCode(rates):
-    byCode = {}
+
+def get_rates_by_code(rates):
+    by_code = {}
     for item in rates:
         code = item["code"]
-        byCode[code] = item
-    return byCode
+        by_code[code] = item
+    return by_code
 
 rates = read_rates()
-ratesByCode = getRatesByCode(rates)
+rates_by_code = get_rates_by_code(rates)
 
 @app.route('/', methods=["GET", "POST"])
 def kalk():
@@ -30,10 +32,10 @@ def conversion():
     currency_code = request.form["currency"]
     quantity = request.form["quantity"]
 
-    bidPrice = ratesByCode[currency_code]["bid"]
+    bid_price = rates_by_code[currency_code]["bid"]
     
-    conv = float(quantity) * bidPrice
-    return render_template("kalk.html", conv=conv, bid=bidPrice, quantity=quantity)
+    conv = float(quantity) * bid_price
+    return render_template("kalk.html", conv=conv, bid=bid_price, quantity=quantity)
 
 if __name__ == "__main__":
     app.run(debug=True)
